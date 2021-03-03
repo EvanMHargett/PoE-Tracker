520275dac911d2ae893262c8252c1ffadba63982
Evanmhargett <evanmhargett@gmail.com>
2021-03-03 12:14:59 -0500

    Added basic components for flips and flips page

---
+++ react-app/src/components/Flip/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Flip(flip){
+
+    const profit = 1
+    const cost = 2
+    const revenue = 3
+    const trades = 4
+
+    return (
+        <div>
+            <div>{profit}</div>
+            <div>{revenue}</div>
+            <div>{trades}</div>
+            <div>{cost}</div>
+        </div>
+    )
+}
+
+export default Flip
---
+++ react-app/src/components/FlipsPage/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Flip from '../Flip'
+
+function FlipsPage(){
+
+    const profit = 1
+    const cost = 2
+    const revenue = 3
+    const trades = 4
+
+    return (
+        <div>
+            <Flip></Flip>
+        </div>
+    )
+}
+
+export default FlipsPage
