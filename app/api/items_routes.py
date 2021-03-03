b7b874b49121cebee1e43ceb3a17e68ce6839303
Evanmhargett <evanmhargett@gmail.com>
2021-03-03 18:36:12 -0500

    Added all relevant types to the items fetch

--- app/api/items_routes.py
+++ app/api/items_routes.py
@@ -3,6 +3,7 @@ from flask_login import login_required, current_user
 from ..config import Config
 from app.models import Flip, Item, db
 import httplib2
+import asyncio
 import json
 
 
@@ -13,6 +14,8 @@ item_routes = Blueprint('items', __name__)
 
 @item_routes.route('/')
 def getAllItems():
+    db.session.execute('TRUNCATE items CASCADE;')
+    db.session.execute("ALTER SEQUENCE items_id_seq RESTART WITH 1")
     resp, content = httplib2.Http().request("https://poe.ninja/api/data/currencyoverview?league=Ritual&type=Currency")
     # print("stuff from request ------------------", resp, content)
     decoded = content.decode('ascii')
@@ -21,13 +24,92 @@ def getAllItems():
     # print("dir ****************", dir(jsonObj))
     item_list = jsonObj["lines"]
     for item in item_list:
-        print("line break ----------------")
-        print(item)
         newItem = Item(
             name=item["currencyTypeName"],
             priceInC=item['chaosEquivalent']
         )
         db.session.add(newItem)
+
+    # await asyncio.sleep(10)
+
+    resp2, content2 = httplib2.Http().request("https://poe.ninja/api/data/itemoverview?league=Ritual&type=DivinationCard")
+    decoded2 = content2.decode('ascii')
+    jsonObj2 = json.loads(decoded2)
+    # print("---------------------", jsonObj["lines"])
+    # print("dir ****************", dir(jsonObj))
+    item_list2 = jsonObj2["lines"]
+    for item in item_list2:
+        newItem = Item(
+            name=item["name"],
+            priceInC=item['chaosValue']
+        )
+        db.session.add(newItem)
+
+    resp2, content2 = httplib2.Http().request("https://poe.ninja/api/data/itemoverview?league=Ritual&type=Prophecy")
+    decoded2 = content2.decode('ascii')
+    jsonObj2 = json.loads(decoded2)
+    # print("---------------------", jsonObj["lines"])
+    # print("dir ****************", dir(jsonObj))
+    item_list2 = jsonObj2["lines"]
+    for item in item_list2:
+        newItem = Item(
+            name=item["name"],
+            priceInC=item['chaosValue']
+        )
+        db.session.add(newItem)
+
+    resp2, content2 = httplib2.Http().request("https://poe.ninja/api/data/itemoverview?league=Ritual&type=UniqueJewel")
+    decoded2 = content2.decode('ascii')
+    jsonObj2 = json.loads(decoded2)
+    # print("---------------------", jsonObj["lines"])
+    # print("dir ****************", dir(jsonObj))
+    item_list2 = jsonObj2["lines"]
+    for item in item_list2:
+        newItem = Item(
+            name=item["name"],
+            priceInC=item['chaosValue']
+        )
+        db.session.add(newItem)
+
+    resp2, content2 = httplib2.Http().request("https://poe.ninja/api/data/itemoverview?league=Ritual&type=UniqueWeapon")
+    decoded2 = content2.decode('ascii')
+    jsonObj2 = json.loads(decoded2)
+    # print("---------------------", jsonObj["lines"])
+    # print("dir ****************", dir(jsonObj))
+    item_list2 = jsonObj2["lines"]
+    for item in item_list2:
+        newItem = Item(
+            name=item["name"],
+            priceInC=item['chaosValue']
+        )
+        db.session.add(newItem)
+
+    resp2, content2 = httplib2.Http().request("https://poe.ninja/api/data/itemoverview?league=Ritual&type=UniqueArmour")
+    decoded2 = content2.decode('ascii')
+    jsonObj2 = json.loads(decoded2)
+    # print("---------------------", jsonObj["lines"])
+    # print("dir ****************", dir(jsonObj))
+    item_list2 = jsonObj2["lines"]
+    for item in item_list2:
+        newItem = Item(
+            name=item["name"],
+            priceInC=item['chaosValue']
+        )
+        db.session.add(newItem)
+
+    resp2, content2 = httplib2.Http().request("https://poe.ninja/api/data/itemoverview?league=Ritual&type=UniqueAccessory")
+    decoded2 = content2.decode('ascii')
+    jsonObj2 = json.loads(decoded2)
+    # print("---------------------", jsonObj["lines"])
+    # print("dir ****************", dir(jsonObj))
+    item_list2 = jsonObj2["lines"]
+    for item in item_list2:
+        newItem = Item(
+            name=item["name"],
+            priceInC=item['chaosValue']
+        )
+        db.session.add(newItem)
+
     db.session.commit()
     return content
     # req = urllib.request.Request(url = 'https://poe.ninja/api/data/currencyoverview?league=Ritual&type=Currency')
