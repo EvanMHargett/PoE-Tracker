45696741ecd16a292846136994f9a8a4556dddf9
Evanmhargett <evanmhargett@gmail.com>
2021-03-08 13:32:46 -0500

    Started form for creating a flip. Having issues with a design that would function

---
+++ react-app/src/components/CreateFlip/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {useSelector} from 'react-redux'
+
+const CreateFlip = () =>{
+    const search = useSelector(state => state.search)
+    return (
+        <div></div>
+    )
+}
+
+export default CreateFlip
