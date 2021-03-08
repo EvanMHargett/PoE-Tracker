64d477fa1f5caf5cce8a286ea3d5192729584f65
Evanmhargett <evanmhargett@gmail.com>
2021-03-08 13:41:16 -0500

    Figured out temporary setup. Want a better user experience, but can figure that out after a working solution exists.

--- react-app/src/components/CreateFlip/index.js
+++ react-app/src/components/CreateFlip/index.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useSelector} from 'react-redux'
 
 const CreateFlip = () =>{
     const search = useSelector(state => state.search)
+    const [input1Id, setInput1Id] = useState('')
+    const [input1Quantity, setInput1Quantity] = useState('')
+    const [input1Id, setInput1Id] = useState('')
+    const [input2Quantity, setInput2Quantity] = useState('')
+    const [outputId, setOutputId] = useState('')
+    const [outputQuantity, setOutputQuantity] = useState('')
     return (
-        <div></div>
+        <div>
+            <form onSubmit={makeFlip}>
+                <input type="number" placeholder="Input 1 ID" value={input1Id} onChange={e => {setInput1Id(e.target.value)}}></input>
+            </form>
+        </div>
     )
 }
 
